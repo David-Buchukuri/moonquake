7e470031046b9ae397ce9dbf4644e8b0f3358508
holmista <tornikebuchukuri@redberry.ge>
2022-10-02 02:03:18 +0400

    feat: moon resize functionality

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -14,10 +14,32 @@ function LandingPage() {
   const [years, setYears] = useState([]);
   const [filteredYears, setFilteredYears] = useState([]);
 
+  const [resize, setResize] = useState(false)
+
+  const myFunction = () => {
+    // your logic here
+    setResize(false)
+  };
+
   useEffect(() => {
     axios
       .get("http://localhost:8000/api/statistics/by-year")
       .then((res) => setYears(res.data.map((el) => el.year)));
+      const keyDownHandler = event => {
+        console.log('User pressed: ', event.key);
+  
+        if (event.key === 'Escape') {
+          event.preventDefault();
+  
+          // 👇️ your logic here
+          myFunction();
+        }
+      };
+  
+      document.addEventListener('keydown', keyDownHandler);
+      return () => {
+        document.removeEventListener('keydown', keyDownHandler);
+      };
   }, []);
 
   const yearsToFilter = () => {
@@ -39,47 +61,54 @@ function LandingPage() {
 
   console.log(showDropdown, showFilterList);
   return (
-    <div className="App">
-      <h1>here goes title</h1>
-      <div className="filter__wrapper">
-        <div ref={chartParent} className="dropdown">
-          <div className="select">
-            <p>chart</p>
-            <i
-              className="arrow down"
-              onClick={() => setShowDropdown(!showDropdown)}
-            ></i>
-          </div>
-          {showDropdown && (
-            <div className="options-wrapper">
-              <Link to="linechart">line chart</Link>
-              <Link to="barchart">bar chart</Link>
+    <div className="App" style={{width:`${resize ? "100vw" : "90vw"}`}}>
+      {resize ? <></> : <div>
+        <h1>here goes title</h1>
+        <div className="filter__wrapper">
+          <div ref={chartParent} className="dropdown">
+            <div className="select">
+              <p>chart</p>
+              <i
+                className="arrow down"
+                onClick={() => setShowDropdown(!showDropdown)}
+              ></i>
             </div>
-          )}
-        </div>
-        <div ref={filterParent} className="dropdown">
-          <div className="select">
-            <p>{filterValue || "Filter"}</p>
-            <i
-              className="arrow down"
-              onClick={() => setShowFilterList(!showFilterList)}
-            ></i>
+            {showDropdown && (
+              <div className="options-wrapper">
+                <Link to="linechart">line chart</Link>
+                <Link to="barchart">bar chart</Link>
+              </div>
+            )}
           </div>
-          {showFilterList && (
-            <div className="options-wrapper">
-              {years.map((el, index) => {
-                return (
-                  <span key={index} onClick={() => filtereYears(el)}>
-                    {el}
-                  </span>
-                );
-              })}
+          <div ref={filterParent} className="dropdown">
+            <div className="select">
+              <p>{filterValue || "Filter"}</p>
+              <i
+                className="arrow down"
+                onClick={() => setShowFilterList(!showFilterList)}
+              ></i>
             </div>
-          )}
+            {showFilterList && (
+              <div className="options-wrapper">
+                {years.map((el, index) => {
+                  return (
+                    <span key={index} onClick={() => filtereYears(el)}>
+                      {el}
+                    </span>
+                  );
+                })}
+              </div>
+            )}
+          </div>
         </div>
-      </div>
+        <div className="resize_button_wrapper" onClick={()=>setResize(!resize)}>
+            <button type="button" className="resize_button">
+              resize
+            </button>
+          </div>
+      </div>}
 
-      <Moon />
+      <Moon widthMultiplier={resize ? 1 : 0.9} heightMultiplier={resize ? 1 : 0.7}/>
     </div>
   );
 }
