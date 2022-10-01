1435ad2afb7d514a2eac2e8ea31251e293e91f1b
batonitamazi <tamazi.mirianashvili.1@btu.edu.ge>
2022-10-01 17:31:04 +0400

    style: tooltip label

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Moon from "./components/Moon";
 
+
 function App() {
-  
+
   return (
     <div className="App">
+      <div className="content">
+
+      </div>
       <Moon />
     </div>
   );
--- src/components/Moon.js
+++ src/components/Moon.js
@@ -3,16 +3,12 @@ import Globe from "react-globe.gl";
 import surface from "../images/8k-lunar-surface.jpg";
 import bumpMap from "../images/lunar-bumpmap.jpg";
 import axios from "axios";
+import '../App.css'
 
 
 function Moon() {
     const [markersData, setMarkersData] = useState([]);
     const [ringData, setRingsData] = useState([]);
-    const labelsTopOrientation = new Set([
-        "Apollo 12",
-        
-    ]);
-
     const fetch = async () => {
         const res = await axios.get(
             "https://test-deployment-production.up.railway.app/api/statistics"
@@ -49,6 +45,8 @@ function Moon() {
     }, []);
     return (
         <Globe
+            width={window.innerWidth * 0.5}
+            height={window.innerHeight}
             globeImageUrl={surface}
             bumpImageUrl={bumpMap}
             backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
@@ -62,18 +60,19 @@ function Moon() {
             pointsMerge={true}
             labelAltitude={0.009}
             labelLabel={(d) => `
-        <div className={ {z-index: 50;} }><b>${d.label}</b></div>
-        <div className={ {z-index: 50;} }>lat: ${d.lat}° --- lng: ${d.lng
-                }°</div>
-        <div className={ {z-index: 50;} }>${d.comment ?? " "}</div>
-        <div className={ {z-index: 50;} }>Happened<i> ${d.date}</i></div>
-      `}
+            <div class='test'>
+                <div><b>${d.label}</b></div>
+                <div>lat: ${d.lat}°</div>
+                <div>lng: ${d.lng}°</div>
+                <div class='comment__text'>${d.comment ?? " "}</div>
+                <div>Happened<i> ${d.date}</i></div>
+            <div>
+            `}
             ringsData={ringData}
             ringColor={() => "#ff0000"}
             ringMaxRadius="maxR"
             ringPropagationSpeed={(d) => d.propagationSpeed}
-            ringRepeatPeriod={(d) => d.repeatPeriod}
-        />
+            ringRepeatPeriod={(d) => d.repeatPeriod} />
     )
 }
 
