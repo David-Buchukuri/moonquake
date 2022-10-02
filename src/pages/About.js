0eef53eba3257f8b906c3c99699e3b34bf81d801
batonitamazi <tamazi.mirianashvili.1@btu.edu.ge>
2022-10-02 13:58:13 +0400

    feat: added audio

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
 import video from "../videos/moon-video.mp4";
 import { Link } from "react-router-dom";
+import ReactAudioPlayer from 'react-audio-player';
 
 export default function About() {
   return (
     <div className="main">
       <div className="overlay"></div>
-      <video src={video} autoPlay loop muted />
+      <ReactAudioPlayer
+        src="/mtvarisMusika.mp3"
+        autoPlay
+        display="none"
+        loop
+      />
+      <video src={video} autoPlay loop muted="false" />
       <div className="content">
         <h1 className="about-h1">Welcome To The</h1>
         <h1 className="about-h1">Moonquake</h1>
